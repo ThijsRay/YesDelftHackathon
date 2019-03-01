cf2b47d4ec5b2543c60c679e34cb67af459a12bc
Raoul <raoulesd@gmail.com>
2019-03-01 01:59:12 +0100

    new timeline shit

---
+++ JSON/test.js
@@ -0,0 +1,29 @@
+var test = [
+  {
+    "e": "johnmayer@shell.com",
+    "c":
+        [
+          {
+            "jessica@student.tudelft.nl": ["Mon, 11 Sep 2000 08:12:00 -0700", "Wed, 13 Sep 2000 08:12:00 -0700"]
+          },
+          {
+            "wienerboy@hotmail.com": ["Fri, 15 Sep 2000 08:12:00 -0700", "Wed, 13 Sep 2000 08:12:00 -0700", "Mon, 11 Sep 2000 08:12:00 -0700"]
+          },
+          {
+            "kalejongen@geenhaar.com": ["Fri, 15 Sep 2000 21:12:00 -0700", "Fri, 15 Sep 2000 12:12:00 -0700", "Fri, 15 Sep 2000 18:12:00 -0700"]
+          }
+        ]
+  },
+  {
+    "e": "arianagrande@bp.com",
+    "c":
+        [
+          {
+            "thijs@student.tudelft.nl": ["Tue, 12 Sep 2000 04:02:00 -0700", "Wed, 13 Sep 2000 08:12:00 -0700"]
+          },
+          {
+            "winterboy@hotmail.com": ["Fri, 15 Sep 2000 08:12:00 -0700", "Wed, 13 Sep 2000 08:12:00 -0700", "Mon, 11 Sep 2000 08:12:00 -0700"]
+          }
+        ]
+  }
+]
